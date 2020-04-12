f9d96c4381c41bf5b858b143088b18a915e8e4a2
jaxonl <lin.jiaxuan@hotmail.com>
2020-04-12 17:35:47 -0400

    add stub for april 10, 11 questions

---
+++ two_sum_and_single_num.py
@@ -0,0 +1,36 @@
+# April 11, 2020 - Daily Interview Pro
+# Hi, here's your problem today. This problem was recently asked
+# by Facebook:
+# Given a list of numbers, where every number shows up twice except
+# for one number, find that one number.
+# Example:
+# Input: [4, 3, 2, 4, 1, 3, 2]
+# Output: 1
+
+# Challenge: Find a way to do this using O(1) memory.
+
+# ^ above was the first question asked in April 2020's 30-day leetcode challenge
+# solution involved using bit-wise XOR (for O(1) memory) to get the last integer.
+
+def single_number(nums):
+    pass
+
+print(single_number([4, 3, 2, 4, 1, 3, 2]))
+# 1
+
+# April 10, 2020 - Daily Interview Pro
+# Hi, here's your problem today. This problem was recently asked
+# by Facebook:
+# You are given a list of numbers, and a target number k. Return
+# whether or not there are two numbers in the list that add up to k.
+# Example:
+# Given [4, 7, 1 , -3, 2] and k = 5,
+# return true since 4 + 1 = 5.
+# Try to do it in a single pass of the list.
+
+def two_sum(lst, k):
+    # Fill this in.
+    pass
+
+print(two_sum([4,7,1,-3,2], 5))
+# True
